33b4f2cf8d09a1a5041e5aedd5f8255f0795aee1
Takeyuki Kojima <unau@users.noreply.github.com>
2015-03-24 06:25:53 +0900

    Add test code

--- test/entry.js
+++ test/entry.js
@@ -1,7 +1,28 @@
 var assert = require('assert');
+var eachize = require('../');
 
-describe('Test', function() {
-  it('first', function() {
-    assert(typeof 'first' == 'string');
+var Album = function() {
+  this.init();
+};
+eachize(Album, 'image');
+Album.prototype.init = function() {
+  this.images = {
+    'first': 'FIRST',
+    'second': 'SECOND',
+  };
+};
+
+var album = new Album();
+
+describe('Album', function() {
+  it('has eachImage', function() {
+    assert(typeof album.eachImage == 'function');
+  });
+  it('eachImage', function() {
+    album.eachImage(function(image, key, x_album) {
+      assert((key == 'first' && image == 'FIRST') ||
+	     (key == 'second' && image == 'SECOND')
+      );
+    });
   });
 });
